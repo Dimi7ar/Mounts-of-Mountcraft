fc6a2b738c063b52cce6150a1e0bc625f7b33acf
Dimi7ar <dimitar.dtrv@gmail.com>
2024-02-16 15:42:56 +0200

    Start

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let goldGain = 0;
+
+function addMount() {
+    let id = getRandomInt(5);
+    const mount = m[id];
+    let card = `<div id="card">
+                        <button onclick=""> 
+                            <div id="name">
+                                ${mount.name} 
+                            </div>
+                            <img src="${mount.image}">
+                            <div id="obtain">
+                                ${mount.desc}
+                            </div>
+                            <div id="income">
+                                +${mount.income} <img src="/images/gold.png" style="width: 12px">/s
+                            </div>
+                        </button>
+                    </div>`;
+    document.getElementById('mount_container').innerHTML += card;
+    goldGain += mount.income;
+    console.log(goldGain)
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
---
+++ js/mounts.js
@@ -0,0 +1,59 @@
+const n = {
+    Invincible: 1,
+    Ashes: 2,
+    TimeLost: 3,
+    HeavenlyOnyx: 4,
+    RavenLord: 5
+},
+r = {
+    Common: 1,
+    Uncommon: 2,
+    Rare: 3,
+    Epic: 4,
+    Legendary: 5
+};
+const i = "/images/";
+const e = ".jpg"
+
+const m = {
+    1: {
+        id: n.Invincible,
+        name: "Invincible",
+        rare: r.Epic,
+        image: `${i}Invincible${e}`,
+        desc: `Icecrown Citadel > Arthas the Lich King`,
+        income: 1
+    },
+    2: {
+        id: n.Ashes,
+        name: "Ashes of Al'ar",
+        rare: r.Epic,
+        image: `${i}Ashes${e}`,
+        desc: `The Eye, Tempest Keep > Kael'thas Sunstrider`,
+        income: 1
+    },
+    3: {
+        id: n.TimeLost,
+        name: "Time-Lost Proto-Drake",
+        rare: r.Epic,
+        image: `${i}TimeLost${e}`,
+        desc: `The Storm Peaks > Time-Lost Proto-Drake`,
+        income: 1
+    },
+    4: {
+        id: n.HeavenlyOnyx,
+        name: "Heavenly Onyx Cloud Serpent",
+        rare: r.Epic,
+        image: `${i}HeavenlyOnyx${e}`,
+        desc: `Kun-Lai Summit > Sha of Anger`,
+        income: 1
+    },
+    5: {
+        id: n.RavenLord,
+        name: "Raven Lord",
+        rare: r.Epic,
+        image: `${i}RavenLord${e}`,
+        desc: `Sethekk Halls > Anzu`,
+        income: 1
+    },
+};
