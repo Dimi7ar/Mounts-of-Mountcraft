3f7ca47223cf60a95d88fa12d569194070c3c238
Dimi7ar <111603656+Dimi7ar@users.noreply.github.com>
2024-02-16 19:20:13 +0300

    Progress

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+let gold = 0;
 let goldGain = 0;
+let collection = [];
 
 function addMount() {
     let id = getRandomInt(5);
     const mount = m[id];
+    if(!collection.includes(mount)) {
+    collection.push(mount);  
     let color = mount.rare == 1 ? `#9d9d9d` : mount.rare == 2 ? `#1eff00` : mount.rare == 3 ? `#0070dd` : mount.rare == 4 ? `#a335ee` : `#ff8000`;
 
     let card = `<div id="card">
@@ -21,9 +25,19 @@ function addMount() {
                     </div>`;
     document.getElementById('mount_container').innerHTML += card;
     goldGain += mount.income;
+    }
     console.log(goldGain)
+}
+
+function updateGold() {
+    gold += goldGain;
+    let htmlGold = `<span style="margin-right: 5px; text-align: right;" id="goldDesc">${gold} gold<br>(+${goldGain}g/s)</span>
+    <div><img src="./images/gold.png" style="width: 25px; margin-top: 5px;"></div>`;
+    document.getElementById(`gold`).innerHTML = htmlGold;
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max) + 1;
 }
+
+setInterval(updateGold, 1000);
